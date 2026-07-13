974baaab0870020199dfa822321e8a1c167ddb46
Alok Singh <singhalok641@gmail.com>
2026-07-13 16:27:23 +0200

    initial commit

---
+++ src/main/java/com/example/splitwise_jul_2026/SplitwiseJul2026Application.java
@@ -0,0 +1,13 @@
+package com.example.splitwise_jul_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseJul2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseJul2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/splitwise_jul_2026/SplitwiseJul2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitwise_jul_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseJul2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
